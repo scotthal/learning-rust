cdb78739cc8f407263a657b579769be5d87927c2
Scott Hallock <scott@hallocks.net>
2021-07-23 18:36:49 +0000

    Type in the pixel_to_point function

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -56,3 +56,36 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     }
     None
 }
+
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 200),
+            (25, 175),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
+        ),
+        Complex {
+            re: -0.5,
+            im: -0.75
+        }
+    );
+}
